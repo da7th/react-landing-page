7333b477776306255f53d774be27c25bcb936e0a
gitcore33 <erickestanislau25@gmail.com>
2026-02-03 18:48:28 -0300

    adds navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,15 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <nav>
-      <div>
-        <a>LANDING PAGE</a>
-        <a href="#">Login</a>
-        <a href="#">Registro</a>
-      </div>
+    <nav className='navbar-container'>
+        <div className='navbar-logo'>
+            <span>LANDING PAGE</span>
+        </div>
+
+        <div className='navbar-menu'>
+            <a href="/" className='login-link'>Login</a>
+            <a href="/" className='register-btn'>Registro</a>
+        </div>
     </nav>
   );
 }
