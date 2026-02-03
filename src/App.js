d4f5d5d7610cc78ce8250b64806566db82704b77
gitcore33 <erickestanislau25@gmail.com>
2026-02-03 19:29:50 -0300

    adds navbar submenu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 // import './App.css';
 import Navbar from "./components/Navbar/Navbar";
+import Menu from "./components/Menu/Menu";
 
 function App() {
   return (
+    <div>
     <Navbar />
+    <Menu />
+    </div>
   );
 }
 
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,17 @@
+import './Menu.css'
+
+function Menu() {
+    return (
+        <div>
+            <nav className="nav-menu">
+                <a href="/" className="menu-category">Categoria 1</a>
+                <a href="/" className="menu-category">Categoria 2</a>
+                <a href="/" className="menu-category">Categoria 3</a>
+                <a href="/" className="menu-category">Categoria 4</a>
+                <a href="/" className="menu-category">Categoria 5</a>
+            </nav>
+        </div>
+    );
+}
+
+export default Menu;
