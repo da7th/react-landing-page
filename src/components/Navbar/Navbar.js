74bd266cac68887b078c949d015f71496f2cac44
gitcore33 <erickestanislau25@gmail.com>
2026-02-03 17:09:08 -0300

    adds navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <a>LANDING PAGE</a>
+        <a href="#">Login</a>
+        <a href="#">Registro</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
