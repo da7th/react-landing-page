8388e13cfce248ba520e5c01133977ffd062109c
gitcore33 <erickestanislau25@gmail.com>
2026-02-03 20:24:20 -0300

    adds sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-// import './App.css';
+import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Menu from "./components/Menu/Menu";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div>
-    <Navbar />
-    <Menu />
+    <div className="app-container">
+      <Navbar />
+      <Menu />
+
+      <div className="main-layout">
+        <Sidebar />
+        <main className="content">
+          <h1>Conteúdo</h1>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,63 @@
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar-div">
+      <nav className="sidebar-nav">
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+        <a href="/" className="x-name">
+          Nome Sobrenome
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default Sidebar;
